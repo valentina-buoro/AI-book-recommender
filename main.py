2f1d690779957aeb41a3b5c2a5a26f549b4796ff
valentina buoro <valentinabuoro4@gmail.com>
2025-08-08 16:07:28 -0400

    removed development server

--- main.py
+++ main.py
@@ -157,7 +157,3 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)"""
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
