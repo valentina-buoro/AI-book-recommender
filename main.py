936d307c763809416a85424f068ac3a09eb89e92
valentina buoro <valentinabuoro4@gmail.com>
2025-08-08 16:05:12 -0400

    reverted chunk size

--- main.py
+++ main.py
@@ -157,3 +157,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)"""
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
